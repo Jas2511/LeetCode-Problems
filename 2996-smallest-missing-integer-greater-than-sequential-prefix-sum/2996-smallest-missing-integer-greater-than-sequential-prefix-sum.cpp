82af9b363cf982ea834f9ca1be047921deb29b66
Jaspreet Singh <jaspreet.25112006@gmail.com>
2026-08-12 10:34:28 +0530

    Time: 0 ms (100%), Space: 22.6 MB (76.81%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int missingInteger(vector<int>& nums) {
-        unordered_map<int, int> p;
-if(nums.size()==1){
-    return nums[0]+1;
-}
-        int y = 0;
-        for (int x : nums) {
+        int n = nums.size();
 
-            p[x] = 1;
-        }
+        // 1. Find sum of longest sequential prefix
         int sum = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] - nums[i - 1] == 1) {
-                sum += nums[i];
-                y = sum;
-            }
 
-            else {
-                y = sum;
-                while (p[y]) {
-                    y++;
-                }
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1)
+                sum += nums[i];
+            else
                 break;
+        }
+
+        // 2. Place values [sum, sum+n-1] at indices [0, n-1]
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= sum &&
+                   nums[i] < sum + n &&
+                   nums[nums[i] - sum] != nums[i]) {
+                
+                swap(nums[i], nums[nums[i] - sum]);
             }
         }
 
-        return y;
+        // 3. First index whose value isn't present
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != sum + i)
+                return sum + i;
+        }
+
+        return sum + n;
     }
 };
